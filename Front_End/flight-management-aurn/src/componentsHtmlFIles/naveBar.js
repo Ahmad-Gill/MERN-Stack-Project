bd591437784670d8c158fcf292ef7c698436f90f
Nimra Amer <nimra.amer2020@gmail.com>
2025-04-03 16:08:37 +0500

    a

--- Front_End/flight-management-aurn/src/componentsHtmlFIles/naveBar.js
+++ Front_End/flight-management-aurn/src/componentsHtmlFIles/naveBar.js
@@ -98,7 +98,7 @@ function NveBar() {
     Analytics
   </Link>
 )}
-            <Link to="/SignUp" onClick={() => setMenuOpen(false)}>SignUp/LogIn</Link>
+            <Link to="/LogIn" onClick={() => setMenuOpen(false)}>LogIn</Link>
 
           </div>
       {/* Search COntainer */}
