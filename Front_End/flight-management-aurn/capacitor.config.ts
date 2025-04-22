92fcf2e1e69faef40ec196b4af32b95b870f82c2
Muhammad Ahmad Gill <113838497+Ahmad-Gill@users.noreply.github.com>
2025-04-22 14:25:09 +0500

    add some adjustment in backend

---
+++ Front_End/flight-management-aurn/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'flight-management-aurn',
+  webDir: 'build'
+};
+
+export default config;
