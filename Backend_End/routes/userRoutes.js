31bd615fd9bede26cc9f64078f5771f8a1d53216
Muhammad Ahmad Gill <113838497+Ahmad-Gill@users.noreply.github.com>
2025-04-24 01:10:03 +0500

    add login and sigunup

--- Backend_End/routes/userRoutes.js
+++ Backend_End/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, createUser } = require('../controllers/userController');
+const { getUsers, signupUser,loginUser } = require('../controllers/userController');
 
-// Define the GET route for /user
-router.get('/', getUsers);  // This should work for the GET request to /user
-router.post('/', createUser);  // This should work for the POST request to /user
+router.get('/', getUsers);
+router.post('/signup', signupUser);
+router.post('/login', loginUser);
 
 module.exports = router;
